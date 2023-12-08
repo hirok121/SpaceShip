466537929041634adec01fa9b7b74bdc823b7337
Hirok_Reza <hirokreza121@gmail.com>
2023-12-09 02:08:22 +0600

    Function approach to OOPs

--- code/main.py
+++ code/main.py
@@ -3,183 +3,170 @@ import os
 import time
 import random
 
-# covert the to class approch from function approch
 
-
-# Time_font = pygame.font.SysFont("comicsans", 30)
-# VEL_font = pygame.font.SysFont("comicsans", 40)
 
 WIDTH ,HEIGHT = 800, 600
 
-# SPACESHIP_VEL = 6
-# FPS = 60
-# BOMB_WIDTH, BOMB_HEIGHT = 6, 14
-# BASE_BOMB_VEL=3
-# BOMB_NUM = 5
-# MAX_BOMBS = 26
-# SPACESHIP_x, SPACESHIP_Y = 40, 60
 
 class Spaceship:
     FPS = 60
-    SPACESHIP_VEL = 6
-    def __init__(self, x, y, width, height, vel, img):
-        self.x = x #not sure yet
-        self.y = y
-        self.width = width
-        self.height = height
-        self.vel = vel
-        self.img = img
-        # Bomb section
-        self.bomb_width, self.bomb_height = 6, 14
-        self.bomb_vel = 3
-        self.bomb_num = 5 # number of bomb added in one time
-        self.max_bomb = 26
+    def __init__(self, win_width, win_height, spaceship_vel=6 ):
+        self.win_width =win_width #window width
+        self.win_height = win_height #window height
+        self.win_rect = pygame.Rect(0, 0, self.win_width, self.win_height)
+        self.hit = False
 
-        #Graphics section
-        self.win=pygame.display.set_mode((WIDTH, HEIGHT))
-        pygame.display.set_caption("Space Game")
-        self.bg_space = pygame.transform.scale(pygame.image.load(self.assets+"bg.jpeg"), (WIDTH, HEIGHT))
-        self.ship = pygame.transform.scale(pygame.image.load(
-            self.assets+"Space-Invaders-Ship.png"), (self.spaceship_x, self.spaceship_y ))
-        bombTemp=pygame.transform.rotate(pygame.image.load(self.assets+"bomb2.png"), 180)
-        self.bomb = pygame.transform.scale(bombTemp, (self.bomb_width+5, self.bomb_height+15))
-
-        # spaceship section
-        self.spaceship_x, self.spaceship_y = 40, 60
+        # function calling
+        pygame.font.init()
 
         # Assets section
         self.assets="assets/"
+        self.check_assets()
         self.Time_font = pygame.font.SysFont("comicsans", 30)
         self.VEL_font = pygame.font.SysFont("comicsans", 40)
-
-        # function calling
-        self.check_assets()
-        pygame.font.init()
 
         #TUNING VARIABLE shouhd be 0 in for standard state
         self.FORWARD_TIME=0 #in second should be 0 in for standard state
         self.REDUCE_INCREMENT_TIME=0
 
-    def draw(self, win):
-        win.blit(self.img, (self.x, self.y))
+        #Time section
+        self.clock = pygame.time.Clock()
+        self.start_time = time.time() - self.FORWARD_TIME
+        self.elapsed_time = 0
+
+        # Bomb section
+        self.bomb_width, self.bomb_height = 6, 14
+        self.bomb_vel = 3
+        self.bomb_num = 5 # number of bomb added in one time
+        self.max_bomb = 26
+        self.bomb_increment_time = 2000-self.REDUCE_INCREMENT_TIME # time interval to add new bomb (in ms) 
+        self.bomb_add_time = 0  # last time bomb added to game
+        self.bomb_passed=0
+        self.bombs = [] # list of bomb rect object
+
+        # spaceship section
+        self.spaceship_vel = spaceship_vel
+        self.spaceship_height, self.spaceship_width = 40, 60 #define it by rect object
+        #spaceship pos bottom center
+        self.spaceship_rect = pygame.Rect( 
+            self.win_width//2-self.spaceship_height//2, self.win_height-self.spaceship_width, self.spaceship_height, self.spaceship_width)
+
+
+        #Graphics section
+        self.win=pygame.display.set_mode(self.win_rect.size)
+        pygame.display.set_caption("Space Game")
+        self.bg_space = pygame.transform.scale(pygame.image.load(self.assets+"bg.jpeg"), self.win_rect.size)
+        self.space_ship = pygame.transform.scale(pygame.image.load(
+            self.assets+"Space-Invaders-Ship.png"), self.spaceship_rect.size)
+        bombTemp=pygame.transform.rotate(pygame.image.load(self.assets+"bomb2.png"), 180)
+        self.bomb = pygame.transform.scale(bombTemp, (self.bomb_width+5, self.bomb_height+15)) #bomb image size bigger than bomb rect size
+
+        # print("Object created")
 
     def check_assets(self):
         # check current directory if its has chid directory assets or it parent directory has assets
-        if not os.path.isdir(assets):
-            if os.path.isdir("../"+assets):
-                assets="../"+assets
+        if not os.path.isdir(self.assets):
+            if os.path.isdir("../"+self.assets):
+                self.assets="../"+self.assets
                 print("assets directory found in parent directory")
             else:
                 print("assets directory not found")
         else:
             print("assets directory found in current directory")
+
+
+    def draw(self):
+        self.time_text = self.Time_font.render(
+        f"Time : {round(self.elapsed_time) }", True, "white")
+        self.win.blit(self.time_text, (5, 5))
+        self.win.blit(self.space_ship, self.spaceship_rect)
+        for bomb in self.bombs:
+            self.win.blit(self.bomb, (bomb.x, bomb.y))
+
+        pygame.display.update()
+        # print("draw function called")
         
-
-
-
-
-# Graphics section
-
-
-def draw_opInfo(bomb_vel=0,ship_vel=0):
-        
-        bomb_vel = VEL_font.render(
-                f"Bomb vel : {bomb_vel:2>.2f}" , True, "white")
-        WIN.blit(bomb_vel, (WIDTH-bomb_vel.get_width(), 5))
-        ship_vel = VEL_font.render(
-                    f"ship vel : {ship_vel:2>.2f}" , True, "white")
-        WIN.blit(ship_vel, (WIDTH-ship_vel.get_width(), bomb_vel.get_height()))
-        Count_bomb = VEL_font.render(
-                    f"BOMB COUNT : {count_bomb:4}", True, "white")
-        WIN.blit(Count_bomb, (WIDTH-Count_bomb.get_width(),Count_bomb.get_height()+ bomb_vel.get_height()))
+    def draw_opInfo(self):
+        """draw operation info on screen
+        read the current value of bomb_vel,ship_vel,bomb_passed and draw on screen
+        """
+        self.bomb_vel_text = self.VEL_font.render(
+                f"Bomb vel : {self.bomb_vel:2>.2f}" , True, "white")
+        self.win.blit(self.bomb_vel_text, (WIDTH-self.bomb_vel_text.get_width(), 5))
+        self.ship_vel_text = self.VEL_font.render(
+                    f"ship vel : {self.spaceship_vel:2>.2f}" , True, "white")
+        self.win.blit(self.ship_vel_text, (WIDTH-self.ship_vel_text.get_width(), self.bomb_vel_text.get_height()))
+        self.bomb_passed_text = self.VEL_font.render(
+                    f"BOMB COUNT : {self.bomb_passed:4}", True, "white")
+        self.win.blit(self.bomb_passed_text, (WIDTH-self.bomb_passed_text.get_width(),self.bomb_passed_text.get_height()+ self.bomb_vel_text.get_height()))
         pygame.display.update()
 
-def draw(spaceship_rect, elapsed_time, bombs):
-    time_text = Time_font.render(
-        f"Time : {round(elapsed_time) }", True, "white")
-    WIN.blit(time_text, (5, 5))
-    WIN.blit(SPACESHIP, (spaceship_rect.x, spaceship_rect.y))
-    for bomb in bombs:
-        WIN.blit(BOMB, (bomb.x, bomb.y))
+        # print("draw_opInfo function called")
 
-    pygame.display.update()
 
-# MAIN GAME
+    def game(self):
+        run = True
 
-def main():
-    run = True
-    clock = pygame.time.Clock()
-    curr_time = time.time() - FORWARD_TIME
-    elapsed_time = 0
-    bomb_increment = 2000-REDUCE_INCREMENT_TIME
-    bomb_add_time = 1500  # last star added to game
-    bombs = []
-    global count_bomb
-    count_bomb=0
+        while run:
+            print(self.clock.tick(Spaceship.FPS))
+            self.bomb_add_time += self.clock.tick(Spaceship.FPS) #self.clock.tick(FPS) return the time in ms since the last call
+            self.elapsed_time = time.time() - self.start_time #time.time() return the time in second since the epoch
+            self.bomb_vel=min(8.5,self.bomb_vel+self.elapsed_time*0.03)
+            self.spaceship_vel=min(7.5,self.spaceship_vel+self.elapsed_time*.01)
 
-    hit = False
+            # print("section 1")
 
-    spaceship_rect = pygame.Rect(
-        WIDTH//2-SPACESHIP_x//2, HEIGHT-SPACESHIP_Y, SPACESHIP_x, SPACESHIP_Y)
+            if self.bomb_add_time > self.bomb_increment_time and len(self.bombs) < self.max_bomb:
+                for _ in range(self.bomb_num):
+                    posX,posY=random.randint(0, WIDTH),random.randint(0, 200)
+                    bomb = pygame.Rect(posX, -posY, self.bomb_width, self.bomb_height)
+                    self.bombs.append(bomb)
 
-    while run:
+                self.bomb_add_time = 0
+                self.bomb_increment_time = max(500, self.bomb_increment_time-50)
 
-        bomb_add_time += clock.tick(FPS)
-        elapsed_time = time.time() - curr_time
-        # elapsed_time=len(bombs)
-        bomb_vel=min(8.5,BASE_BOMB_VEL+elapsed_time*.03)
-        spaceship_vel=min(7.5,SPACESHIP_VEL+elapsed_time*.01)
+            # print("section 2")
 
-        
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                    pygame.quit()
 
-        if bomb_add_time > bomb_increment and len(bombs) < MAX_BOMBS:
-            for _ in range(BOMB_NUM):
-                starX = random.randint(0, WIDTH)
-                starY = random.randint(0, 200)
-                bomb = pygame.Rect(starX, -starY, BOMB_WIDTH, BOMB_HEIGHT)
-                bombs.append(bomb)
+            keys_pressed = pygame.key.get_pressed()
+            if keys_pressed[pygame.K_LEFT] and self.spaceship_rect.x > 0:
+                self.spaceship_rect.x -= self.spaceship_vel
+            if keys_pressed[pygame.K_RIGHT] and self.spaceship_rect.x + self.spaceship_rect.width < WIDTH:
+                self.spaceship_rect.x += self.spaceship_vel
 
-            bomb_add_time = 0
-            bomb_increment = max(300, bomb_increment-50)
+            # print("section 3")
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
+            # moving bombs
+            for bomb in self.bombs:
+                bomb.y += self.bomb_vel
+                if bomb.y > HEIGHT:
+                    self.bombs.remove(bomb)
+                    self.bomb_passed+=1
+                elif bomb.y + bomb.height >= self.spaceship_rect.y and bomb.colliderect(self.spaceship_rect):
+                    self.bombs.remove(bomb)
+                    self.hit = True
+                    break
 
-        keys_pressed = pygame.key.get_pressed()
-        if keys_pressed[pygame.K_LEFT] and spaceship_rect.x > 0:
-            spaceship_rect.x -= spaceship_vel
-        if keys_pressed[pygame.K_RIGHT] and spaceship_rect.x + spaceship_rect.width < WIDTH:
-            spaceship_rect.x += spaceship_vel
+            # print("section 4")
 
-    # moving stars
-        for bomb in bombs:
-            bomb.y += bomb_vel
-            if bomb.y > HEIGHT:
-                bombs.remove(bomb)
-                count_bomb+=1
-            elif bomb.y + bomb.height >= spaceship_rect.y and bomb.colliderect(spaceship_rect):
-                bombs.remove(bomb)
-                hit = True
+            if self.hit:
+                lost_text =self.Time_font.render("You Lost!", 1, "white")
+                self.win.blit(lost_text, (self.win_width/2 - lost_text.get_width()/2, self.win_height/2 - lost_text.get_height()/2))
+                pygame.display.update()
+                pygame.time.delay(2000)
                 break
 
-        if hit:
-            lost_text =Time_font.render("You Lost!", 1, "white")
-            WIN.blit(lost_text, (WIDTH/2 - lost_text.get_width()/2, HEIGHT/2 - lost_text.get_height()/2))
-            pygame.display.update()
-            pygame.time.delay(2000)
-            break
+            #DRAWING staf
+            self.win.blit(self.bg_space, (0, 0))
+            self.draw_opInfo()
+            self.draw()
 
-        #DRAWING staf
-        WIN.blit(SPACE, (0, 0))
-        draw_opInfo(bomb_vel,spaceship_vel)
-        draw(spaceship_rect, elapsed_time, bombs)
-
-        
-
-    main()
-
+            # print("section 5")
 
 if __name__ == "__main__":
-    main()
+    Obj=Spaceship(WIDTH,HEIGHT)
+    Obj.game()
